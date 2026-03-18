91fa52352458d3456fb9c4e386edaa1e5d80806d
DaoNQ1408 <nguyenquangdao1408@gmail.com>
2026-03-19 03:08:19 +0700

    init java microservice

---
+++ awesome-java/core-scout-service/src/main/java/com/daonq/corescoutservice/CoreScoutServiceApplication.java
@@ -0,0 +1,13 @@
+package com.daonq.corescoutservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreScoutServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreScoutServiceApplication.class, args);
+    }
+
+}
---
+++ awesome-java/core-scout-service/src/test/java/com/daonq/corescoutservice/CoreScoutServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daonq.corescoutservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreScoutServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
