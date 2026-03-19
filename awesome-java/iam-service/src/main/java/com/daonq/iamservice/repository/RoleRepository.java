32aa22d4e12136df554934853719cd6cc585ade8
DaoNQ1408 <nguyenquangdao1408@gmail.com>
2026-03-19 18:26:26 +0700

    reinstall the project and setup eureka server, api gateway

--- awesome-java/iam-service/src/main/java/com/daonq/iamservice/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.daonq.iamservice.repository;
-
-import com.daonq.iamservice.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Long> {
-}
